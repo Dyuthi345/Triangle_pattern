219ac48108ecdee48617355149aa587d30f0e405
Dyuthi345 <149928902+Dyuthi345@users.noreply.github.com>
2023-11-05 14:13:56 +0530

    Add project files.

---
+++ triangle_pattern/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class Trianglepattern
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the height of the Triangle : ");
+        if (int.TryParse(Console.ReadLine(), out int height))
+        {
+            PrintTriangle(height); // Print the triangle 
+        }
+        else
+        {
+            Console.WriteLine("Please enter a valid integer for height");
+        }
+    }
+    static void PrintTriangle(int height)  
+    {
+        for (int i = 1; i<= height; i++)
+        {
+            for(int j = 1; j<= height - i; j++) // print spaces before the stars
+            {
+                Console.Write(" ");
+            }
+            for(int k = 1; k<= 2*i - 1; k++)  // print stars for each row
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine(); // move to the next line for the next row
+        }
+    }
+}
